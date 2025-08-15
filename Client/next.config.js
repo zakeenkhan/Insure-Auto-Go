7dc093b8a4777795e74fa33df1f1477559078e04
Zakeen khan <zakeenkhann@gmail.com>
2025-08-15 18:21:09 +0500

    resolve errors,

---
+++ Client/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
+  // Add any other Next.js config options here
+}
+
+module.exports = nextConfig
